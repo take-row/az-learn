6c63db994a3ca14ffc17845454ab25745e4892fd
富永拓朗 <tominagatakurou@TTnoMCBA.local>
2025-04-26 11:48:52 +0900

    test

---
+++ createTask/create_task.py
@@ -0,0 +1,37 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+import os
+
+org = os.environ["ORG_NAME"]          # 例: "myorg"
+project = os.environ["PROJECT_NAME"]  # 例: "MyProject"
+pat = os.environ["AZURE_DEVOPS_PAT"]
+
+url = f"https://dev.azure.com/{org}/{project}/_apis/wit/workitems/$Task?api-version=7.0"
+
+headers = {
+    "Content-Type": "application/json-patch+json"
+}
+
+payload = [
+    {
+        "op": "add",
+        "path": "/fields/System.Title",
+        "value": "Python経由で作成された作業項目"
+    },
+    {
+        "op": "add",
+        "path": "/fields/System.Description",
+        "value": "この作業項目はPythonスクリプトから生成されました。"
+    }
+]
+
+response = requests.post(
+    url,
+    auth=HTTPBasicAuth('', pat),
+    headers=headers,
+    data=json.dumps(payload)
+)
+
+print("Status Code:", response.status_code)
+print("Response:", response.json())
